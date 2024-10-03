059900d845b2f2abd418051d78dc2962b92a5f91
arodriguezgim <arodriguezg@iesch.org>
2024-10-03 16:41:46 +0200

    Llevo valores a la vista detalle

--- 2-Android/02_RegistrodeSuperHeroes/app/src/main/java/org/iesch/a02_registrodesuperheroes/RegisterActivity.kt
+++ 2-Android/02_RegistrodeSuperHeroes/app/src/main/java/org/iesch/a02_registrodesuperheroes/RegisterActivity.kt
@@ -5,17 +5,54 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import org.iesch.a02_registrodesuperheroes.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         // 9 - binding
-        setContentView(R.layout.activity_register)
+        val binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // 10 - Nos creamos una variable bundle que recoge todos los valores traídos.
+        val bundle:Bundle = intent.extras!!
+        // 11 - Recoger cada uno de los valores del bundle
+        // Estos valores pueden ser nulos. Lo solucionamos mediante el operador Elvis ?:
+        val superHeroName = bundle.getString("heroName") ?: "Sin Nombre"
+        val alterEgo = bundle.getString("alter_ego") ?:  ""
+        val bio = bundle.getString("bio") ?: ""
+        val power = bundle.getFloat("power") ?: 0.0
+
+        // 12 - Los mostramos en pantalla
+        binding.heroName.text = superHeroName.toString()
+        binding.alterEgoText.text = alterEgo.toString()
+        binding.bioText.text = bio.toString()
+        binding.ratingBar.rating = power.toFloat()
+
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
