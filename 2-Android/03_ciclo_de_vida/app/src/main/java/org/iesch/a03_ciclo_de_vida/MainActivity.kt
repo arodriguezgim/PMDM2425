bfbc4ae0ffdc7b888508661c907d4196740abd48
arodriguezgim <arodriguezg@iesch.org>
2024-10-08 18:24:43 +0200

    App 03 terminada

--- 2-Android/03_ciclo_de_vida/app/src/main/java/org/iesch/a03_ciclo_de_vida/MainActivity.kt
+++ 2-Android/03_ciclo_de_vida/app/src/main/java/org/iesch/a03_ciclo_de_vida/MainActivity.kt
@@ -1,6 +1,7 @@
 package org.iesch.a03_ciclo_de_vida
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +17,37 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        Log.i("CICLO_DE_VIDA", "Entramos en el método onCreate")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i("CICLO_DE_VIDA", "Entramos en el método onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i("CICLO_DE_VIDA", "Entramos en el método onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i("CICLO_DE_VIDA", "Entramos en el método onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i("CICLO_DE_VIDA", "Entramos en el método onStop")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.i("CICLO_DE_VIDA", "Entramos en el método onRestart")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("CICLO_DE_VIDA", "Entramos en el método onDestroy")
     }
 }
