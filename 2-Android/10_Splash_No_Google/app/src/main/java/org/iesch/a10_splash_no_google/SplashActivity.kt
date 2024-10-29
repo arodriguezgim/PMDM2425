09780343ade68c2e13e61440c8b46a15fc7610f6
arodriguezgim <arodriguezg@iesch.org>
2024-10-29 17:31:58 +0100

    Fin APP No Recomendada

---
+++ 2-Android/10_Splash_No_Google/app/src/main/java/org/iesch/a10_splash_no_google/SplashActivity.kt
@@ -0,0 +1,27 @@
+package org.iesch.a10_splash_no_google
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }, 3000)
+    }
+}
