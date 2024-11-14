4f3efb4f5671c70f5c51bcd304a640626db82d2d
arodriguezgim <arodriguezg@iesch.org>
2024-11-14 16:51:43 +0100

    App Google Maps Finalizada

--- 2-Android/15_Google_Maps/app/src/main/java/org/iesch/a15_google_maps/MainActivity.kt
+++ 2-Android/15_Google_Maps/app/src/main/java/org/iesch/a15_google_maps/MainActivity.kt
@@ -14,15 +14,23 @@ import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.CustomCap
+import com.google.android.gms.maps.model.Dash
+import com.google.android.gms.maps.model.Dot
+import com.google.android.gms.maps.model.Gap
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.Polyline
+import com.google.android.gms.maps.model.PolylineOptions
+import com.google.android.gms.maps.model.RoundCap
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMyLocationButtonClickListener, GoogleMap.OnMyLocationClickListener {
 
-    // 1 - Nos creamos la variable que será nuestro mapa
+
     private lateinit var map : GoogleMap
 
-    // Creo la  constante para el codigo
+
     companion object {
         private const val LOCATION_PERMISSION_REQUEST_CODE = 1
     }
@@ -39,15 +47,55 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMyLoca
 
         createFragment()
     }
-    // 2 - createFragment creará el mapa
+
     private fun createFragment() {
         val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
     }
-    // 3 - Este metoso se llamará cuando el mapa haya sido creado
+
+    // 2
+
+    private fun createPolylines() {
+        val polylinesOptions = PolylineOptions()
+            .add(LatLng(40.35326928818816,-1.10872386116381 ))
+            .add(LatLng(40.341787196327715,-1.1153078441756463 ))
+            .add(LatLng(40.3276087956057,-1.1069706639350727 ))
+            .add(LatLng(40.32642885176733,-1.0915317577413646 ))
+            .add(LatLng(40.34256434412808,-1.0966072458418807 ))
+            .add(LatLng(40.35326928818816,-1.10872386116381 ))
+            .width(30f)
+            .color(ContextCompat.getColor(this, R.color.polilyne_color))
+
+        val polyline = map.addPolyline(polylinesOptions)
+        polyline.startCap = RoundCap()
+        // polyline.endCap = CustomCap(BitmapDescriptorFactory.fromResource(R.drawable.indice))
+
+        val pattern = listOf(
+            Dot(), Gap(10F), Dash(50F), Gap(10F)
+        )
+        polyline.pattern = pattern
+
+        polyline.isClickable = true
+        map.setOnPolylineClickListener {
+            cambiarColor(polyline)
+        }
+    }
+
+    private fun cambiarColor(polyline: Polyline) {
+        val color = (0..3).random()
+        when(color){
+            0-> polyline.color = ContextCompat.getColor(this, R.color.polilyne_color)
+            1-> polyline.color = ContextCompat.getColor(this, R.color.polilyne_color_2)
+            2-> polyline.color = ContextCompat.getColor(this, R.color.polilyne_color_3)
+            3-> polyline.color = ContextCompat.getColor(this, R.color.polilyne_color_4)
+        }
+    }
+
     override fun onMapReady(googleMap: GoogleMap) {
         map = googleMap
+        // 1
         createMarker()
+        createPolylines()
         map.setOnMyLocationButtonClickListener(this)
         map.setOnMyLocationClickListener(this)
         enableLocation()
@@ -65,31 +113,30 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMyLoca
         )
     }
 
-    // 4 - Funcion para saber si tenemos los permisos o no
+
     private fun isLocationPermissionGranted() = ContextCompat
         .checkSelfPermission( this,
             android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
 
-    // 5 Creamos una funcion que compruebe esto
+
     private fun enableLocation(){
-        // si todavia no se ha creado el mapa
+
         if (!::map.isInitialized) return
         if ( isLocationPermissionGranted()){
-            // si que tiene permiso
+
             map.isMyLocationEnabled = true
         } else {
             requestLocationPermission()
         }
     }
-    // Esta funcion es para pedir los permisos de Localizacion
+
     private fun requestLocationPermission() {
-        // Lógica para que el usuario acepte el permiso
+
         if ( ActivityCompat.shouldShowRequestPermissionRationale(this, android.Manifest.permission.ACCESS_FINE_LOCATION)){
-            // si entro por aqui será porque ya le habíamos pedido los permisos y los rechazó
-            // Mostramos una alerta para que acepte los permisos
+
             Toast.makeText(this, "Ve a ajustes y acepta los permisos de ubicación", Toast.LENGTH_SHORT).show()
         } else {
-            //Pedimos el permiso
+
             ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_PERMISSION_REQUEST_CODE)
         }
     }
